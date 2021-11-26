031e14ca4109b50cde782542ee5b58137f526ebe
Yoshiki Takagi <yoshiki.tkg@gmail.com>
2021-11-27 02:43:09 +0900

    add React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,59 @@
 import React from 'react';
+import { Routes, Route, Outlet, Link } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+        </Route>
+      </Routes>
     </div>
+  );
+}
+
+function Layout() {
+  return (
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+
+      <Outlet />
+    </header>
+  );
+}
+
+function Home() {
+  return (
+    <>
+      <main>
+        <h2>Welcome to the homepage!</h2>
+        <p>You can do this, I believe in you.</p>
+      </main>
+      <nav>
+        <Link className="App-link" to="/about">About</Link>
+      </nav>
+    </>
+  );
+}
+
+function About() {
+  return (
+    <>
+      <main>
+        <h2>Who are we?</h2>
+        <p>
+          That feels like an existential question, don't you
+          think?
+        </p>
+      </main>
+      <nav>
+        <Link className="App-link" to="/">Home</Link>
+      </nav>
+    </>
   );
 }
 
