ed17f11419aa03ee6c06fadc544af03cd04d0292
Yoshiki Takagi <yoshiki.tkg@gmail.com>
2023-01-16 00:19:30 +0900

    fix testing

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders top page', () => {
+  render(<App />, { wrapper: BrowserRouter });
+  expect(screen.getByText(/Hello World!/i)).toBeInTheDocument();
 });
