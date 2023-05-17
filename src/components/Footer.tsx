c7927ca0d9fab475269bda45f935e3d435cf5305
Yoshiki Takagi <yoshiki.tkg@gmail.com>
2023-05-17 09:46:43 +0900

    Update Footer component to display current year dynamically

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,10 @@
 function Footer() {
+  const year = new Date().getFullYear();
+
   return (
     <div className="flex justify-center items-center h-12 bg-black text-white font-mono">
-      <p className="md:block hidden">Copyright &copy; 2021 takagi.dev All rights reserved.</p>
-      <p className="md:hidden">&copy; 2021 takagi.dev</p>
+      <p className="md:block hidden">Copyright &copy; {year} takagi.dev All rights reserved.</p>
+      <p className="md:hidden">&copy; {year} takagi.dev</p>
     </div>
   );
 }
